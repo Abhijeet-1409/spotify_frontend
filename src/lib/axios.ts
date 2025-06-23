a24dc2fd7b5480bc31a0cd67e41bc8662ba90848
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-06-23 15:37:15 +0530

    Add axios

---
+++ src/lib/axios.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+    baseURL: "http://localhost:8000/api",
+});
+
+export const updateApiToken = (token: string | null) => {
+	if (token) axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+	else delete axiosInstance.defaults.headers.common["Authorization"];
+};
