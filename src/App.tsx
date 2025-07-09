660a4954506f28e784ed4f34f0784ff3ff1f6fb2
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-07-09 23:10:57 +0530

    Add search icon to the leftbar and add route for search in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import ChatPage from "./pages/chat/ChatPage";
 import AlbumPage from "./pages/album/AlbumPage";
 import AdminPage from "./pages/admin/AdminPage";
 import NotFoundPage from "./pages/404/NotFoundPage";
+import SearchPage from "./pages/search/SearchPage";
 import { Toaster } from "react-hot-toast";
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
         <Route element={<MainLayout />}>
 					<Route path='/' element={<HomePage />} />
 					<Route path='/chat' element={<ChatPage />} />
+          <Route path='/search' element={<SearchPage/>} />
 					<Route path='/albums/:albumId' element={<AlbumPage />} />
 					<Route path='*' element={<NotFoundPage />} />
 				</Route>
